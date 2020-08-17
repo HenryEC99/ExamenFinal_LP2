da484bb9577b8ccd1cbf54918526df17c09e2835
Gloria Ruiz <2017220016@untels.edu.pe>
2020-08-17 05:12:57 -0500

    datos3 CARRERA Gloria Ruiz

---
+++ src/java/Interfaces/CRUDcarrera.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import Modelo.Carrera;
+import java.util.List;
+
+/**
+ *
+ * @author TuNombre
+ */
+public interface CRUDcarrera {
+    public List listarcarrera(); // listar
+    public Carrera buscarcarrera(int idcarrera); // buscar
+    public boolean agregarcarrera(Carrera carrera); // agregar
+    public boolean editarcarrera(Carrera carrera); // editar
+    public boolean eliminarcarrera(int idcarrera); // eliminar
+}
